acf32fe5835d4ed85f3f38fa567f11601ceb0dfc
CaNavas <navasochoa@hotmail.com>
2023-11-01 15:18:15 -0500

    más php

---
+++ 04cookies/pedir-idioma.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies en PHP</title>
+</head>
+<body>
+    <a href="grabar-cookie.php?idioma=es">versión en español</a>
+    <br>
+    <a href="grabar-cookie.php?idioma=en">version in English</a>
+</body>
+</html>
